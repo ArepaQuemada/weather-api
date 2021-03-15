94c28167218ba7bf3e1f8f51361e87c9b756b828
Juan <juan.rigual>
2021-03-15 00:21:08 -0300

    fix: fix middleware response

--- src/loaders/middlewares.ts
+++ src/loaders/middlewares.ts
@@ -11,7 +11,7 @@ export default (app: Express) => {
   app.use(cors())
   app.use(express.json())
   app.get("/", (req, res) => {
-    res.status(200)
+    res.status(200).send({})
   })
   app.use("/v1/location", location)
   app.use("/v1/current", current)
